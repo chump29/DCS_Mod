4c7ad88065f52e576f227bc017feb497876501e3
chump29 <chump@29thdiv.com>
2020-06-28 19:38:07 -0500

    Bug fixes

--- honkZone.lua
+++ honkZone.lua
@@ -23,18 +23,19 @@ do
 			if unit and unit:getLife() > 1 then
 				local unitName = unit:getName()
 				local u = UNIT:FindByName(unitName)
-				if u then
+				if unitName and u then
 					for _, zone in ipairs(honkZone.zones) do
-						if u:IsInZone(ZONE:FindByName(zone)) then
-							if not honkZone.whosInZone[unitName] then
-								honkZone.whosInZone[unitName] = true
+						local z = ZONE:FindByName(zone)
+						if z and u:IsInZone(z) then
+							if honkZone.whosInZone[unitName] ~= zone then
+								honkZone.whosInZone[unitName] = zone
 								local g = u:GetGroup()
 								if g then
 									trigger.action.outSoundForGroup(g:GetID(), "l10n/DEFAULT/" .. honkZone.sound)
 								end
 							end
 						else
-							if honkZone.whosInZone[unitName] then
+							if honkZone.whosInZone[unitName] == zone then
 								honkZone.whosInZone[unitName] = nil
 							end
 						end

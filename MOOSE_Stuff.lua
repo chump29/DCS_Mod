ed316fa374c9e041f6b4a91e6bd384749a5aae71
chump29 <chump@29thdiv.com>
2020-07-02 13:40:42 -0500

    Added more carrier vehicles

--- MOOSE_Stuff.lua
+++ MOOSE_Stuff.lua
@@ -1,17 +1,35 @@
-local base = _G
-local assert = base.assert
+--[[
+-- MOOSE Stuff
+-- by Chump
+--]]
 
 assert(BASE ~= nil, "Must include MOOSE framework for this script to work!")
 
-local missileTrainer = MISSILETRAINER
+MISSILETRAINER
 	:New(200)
-	:InitTrackingFrequency(-2)
+	:InitTrackingFrequency(-1)
 	:InitMenusOnOff(false)
 
-local pseudoAtc = PSEUDOATC
+PSEUDOATC
 	:New()
 	:Start()
 
-rescueHelo = RESCUEHELO -- NOTE: global on purpose!
-	:New(UNIT:FindByName("Carrier"), "RescueHelo")
+RESCUEHELO -- NOTE: global on purpose!
+	:New(UNIT:FindByName("Carrier"), "CSAR")
+	:Start()
+
+RECOVERYTANKER
+	:New(UNIT:FindByName("Carrier"), "Tanker")
+	:SetAltitude(6000) -- in ft
+	:SetRadio(243)
+	:SetTACAN(79, "TEX")
+	:Start()
+
+RECOVERYTANKER
+	:New(UNIT:FindByName("Carrier"), "AWACS")
+	:SetAltitude(20000) -- in ft
+	:SetAWACS()
+	:SetRadio(255)
+	:SetTACAN(55, "WAX")
+	:SetTakeoffAir()
 	:Start()

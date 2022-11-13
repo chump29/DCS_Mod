835805537f71fb98aa07287d76751b5abbd7e5b3
chump29 <chump@29thdiv.com>
2022-11-13 17:23:44 -0600

    Increase tower ID range

--- Radio_Tower.lua
+++ Radio_Tower.lua
@@ -107,7 +107,7 @@ do
 					stations = string.format("%s%s - %.3f %s %s [%dw]", stations, station.name, getFrequency(station.frequency), getHertz(station.frequency), getModulation(station.modulation), math.floor(math.abs(station.power)))
 				end
 				if string.len(stations) > 0 then
-					tower.id = math.random(1, 1000000)
+					tower.id = math.random(100000, 1000000)
 					trigger.action.markToAll(tower.id, string.format("%s stations:\n%s", tower.name, stations), zone.point, true)
 				end
 			end

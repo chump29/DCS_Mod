3716eb874693e2bead31cdb45b6c0cb0a922544a
chump29 <chris@postfmly.com>
2023-06-10 13:04:17 -0500

    Added names

--- Briefing Addons/theatre_data.lua
+++ Briefing Addons/theatre_data.lua
@@ -5,42 +5,42 @@
 
 return {
 	["Caucasus"] = {
-		["icao"] = "UGTB",
+		["icao"] = "UGTB", -- Tbilisi
 		["utc"] = 4,
 		["position"] = { x = -314926.25, y = 479.69479370117, z = 895724 }
 	},
 	["Falklands"] = {
-		["icao"] = "EGYP",
+		["icao"] = "EGYP", -- Mount Pleasant
 		["utc"] = -3,
 		["position"] = { x = 73598.5625, y = 74.136428833008, z = 46176.4140625 }
 	},
 	["MarianaIslands"] = {
-		["icao"] = "PGUA",
+		["icao"] = "PGUA", -- Andersen
 		["utc"] = 10,
 		["position"] = { x = 9961.662109375, y = 166.12214660645, z = 13072.155273438 }
 	},
 	["Nevada"] = {
-		["icao"] = "KLSV",
+		["icao"] = "KLSV", -- Nellis
 		["utc"] = -8,
 		["position"] = { x = -399275.84375, y = 561.30914306641, z = -18183.12109375 }
 	},
 	["Normandy"] = {
-		["icao"] = "LFPO",
+		["icao"] = "LFPO", -- Orly
 		["utc"] = 2,
 		["position"] = { x = -73843.789062424, y = 83.001083923387, z = 200480.84375 }
 	},
 	["PersianGulf"] = {
-		["icao"] = "OMAA",
+		["icao"] = "OMAA", -- Abu Dhabi
 		["utc"] = 4,
 		["position"] = { x = -187211.25, y = 28.000028610229, z = -163535.515625 }
 	},
 	["Sinai"] = {
-		["icao"] = "HECA",
+		["icao"] = "HECA", -- Cairo
 		["utc"] = 3,
 		["position"] = { x = 6618.8779297633, y = 120.00110681157, z = 18463.62890625 }
 	},
 	["Syria"] = {
-		["icao"] = "LCLK",
+		["icao"] = "LCLK", -- Larnaka
 		["utc"] = 3,
 		["position"] = { x = -8466.0517578125, y = 5.0000047683716, z = -209773.46875 }
 	}
